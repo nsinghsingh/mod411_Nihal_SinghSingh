89b1cc24d3196193ea719d107b4b2241507d5e62
nsinghsingh <qweras%7>
2019-10-17 10:01:46 +0200

    Made insertion sort

---
+++ src/main/java/InsertionSort.java
@@ -0,0 +1,22 @@
+
+public class InsertionSort {
+
+    public void insertionsort(int[] numbers) {
+        for (int i = 1; i < numbers.length; i++) {
+            int value = numbers[i];
+            int j = i;
+            while ((j > 0) && (numbers[j - 1] > value)) {
+                numbers[j] = numbers[j - 1];
+                j = j - 1;
+                numbers[j] = value;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] liste = {0, 9, 4, 6, 2, 8, 5, 1, 7, 3};
+        InsertionSort sort = new InsertionSort();
+        sort.insertionsort(liste);
+        for (int value : liste) System.out.print(value + " ");
+    }
+}
