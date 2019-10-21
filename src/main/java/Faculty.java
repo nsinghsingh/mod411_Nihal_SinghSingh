5547d2ccd4edcc3f3029f8595157c95fea9d20e6
nsinghsingh <qweras%7>
2019-10-21 09:52:26 +0200

    Faculty mit besseren Datentyp geschrieben

--- src/main/java/Faculty.java
+++ src/main/java/Faculty.java
@@ -4,12 +4,12 @@ public class Faculty {
 
     public static void main(String[] args) {
 
-        Function<Integer, Integer> faculty = (input) -> {
-            for (int i = input - 1; i > 0; i--) {
+        Function<Long, Long> faculty = (input) -> {
+            for (long i = input - 1; i > 0; i--) {
                 input = input * i;
             }
             return input;
         };
-        System.out.println(faculty.apply(10));
+        System.out.println(faculty.apply((long) 12));
     }
 }
